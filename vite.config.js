fbd6f7594ea7dbee581ab046d1ac814b68eeecb0
jinsoul75 <wlsthf75@gmail.com>
2025-03-26 22:33:47 +0900

    chore: base url 설정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter1-1/" : "",
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
   },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
+  spa: true,
 });
